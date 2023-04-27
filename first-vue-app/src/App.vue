6776ef09e9d9918cabe480d94ccbbe45d23bf10c
yusuke-miura <miura2000T@outlook.jp>
2023-04-27 11:23:55 +0900

    vue仕様確認

--- first-vue-app/src/App.vue
+++ first-vue-app/src/App.vue
@@ -1,13 +1,24 @@
 <!-- JavaScript部分 -->
 <script setup>
 console.log('Hello World');
+let message = 'Hello world'
+// const message = '<h2>Hello world</h2>'
+
+const upperCase = () =>{
+  message = message.toUpperCase();
+};
+
+upperCase();
+
 </script>
 
 <!-- html部分 -->
 <template>
   <div>
     <h1>Vue 3 入門</h1>
-    <h1></h1>
+    <p>{{ message }}</p>
+    <div v-text="message"></div>
+    <div v-html="message"></div>
     <!-- <h1> -->
   </div>
 </template>
