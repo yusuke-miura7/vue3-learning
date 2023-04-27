bdab6e354229479ba5123e6f3d0f5c4fc844c055
yusuke-miura <miura2000T@outlook.jp>
2023-04-27 14:58:15 +0900

    v-forの確認

--- first-vue-app/src/App.vue
+++ first-vue-app/src/App.vue
@@ -15,6 +15,18 @@ const link = 'https://google.com';
 const isActive = true;
 const isBlack = true;
 
+let stock = 5;
+const increment=() =>{
+  console.log(stock);
+  stock+=1;
+}
+
+const users = [
+  { id: 1, name: 'John Doe', email: 'john@test.com', admin: true },
+  { id: 2, name: 'Jane Doe', email: 'jane@example.com', admin: false },
+  { id: 3, name: 'Kevin MacDonald', email: 'kevin@test.com', admin: false },
+];
+
 </script>
 
 <!-- html部分 -->
@@ -22,19 +34,35 @@ const isBlack = true;
   <div>
     <h1>Vue 3 入門</h1>
     <p>{{ message }}</p>
-    <div v-text="message"></div>
-    <div v-html="message"></div>
-  <div>
+    <div v-text="message">
+    </div>
+    <div v-html="message">
+    </div>
+  
     <a v-bind:href="link">google</a>
     <!-- v-bindの確認 -->
     <p class="active">v-bindの設定方法の確認</p>
     <!-- 動的なクラス適用 -->
-    <p class="{active: isActive }">v-bindの設定方法の確認</p>
+    <p :class="{active: isActive }">v-bindの設定方法の確認2</p>
     <!-- 複数クラスの適用 -->
     <p class="underLine" :class="{active:isActive, back:isBlack}">
     v-bindの設定方法の確認
     </p>
-  </div>
+
+    <!-- v-ifの確認 -->
+    <div v-if="stock >5">まだ商品に在庫数に余裕があります</div>
+    <div v-else-if="stock === 0">申し訳ありません。現在売り切れです。</div>
+    <div v-else>在庫数が少なくなっております。お急ぎください</div>
+
+    <!-- v-forの確認 -->
+    <div>
+    <p v-for="user in users" :key="user.id">{{ user.id }}:{{ user.name }}({{ user.email }})</p>
+    <p v-for="(user, index) in users" :key="user">
+    {{ index }}-{{ user.id }}{{ user.name }}{{ user.email }}</p>
+    </div>
+    
+    <button @click="increment">在庫を増やす</button>
+  
   </div>
 </template>
 
